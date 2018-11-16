a5016f497c70e45b443d240109e3ae24ce0f3f94
danielackuaku <dma2@students.calvin.edu>
2018-11-16 16:25:48 -0500

    Java doc implementaion for homework1 + properly formatted code.

--- Homework1/app/src/main/java/dma2/cs262/calvin/edu/homework1/MainActivity.java
+++ Homework1/app/src/main/java/dma2/cs262/calvin/edu/homework1/MainActivity.java
@@ -1,27 +1,32 @@
 package dma2.cs262.calvin.edu.homework1;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
+/**
+ * Returns an integer result of the mathematical operation performed on two numbers.
+ * <p>
+ * This method always checks to make sure both values are not null,
+ * and are whole numbers. When the inputs are not present the calculate button does not
+ * calculate the result.
+ *
+ * param   value1 and value2 the numbers to calculated that need to be calculated.
+ */
 public class MainActivity extends AppCompatActivity {
-     EditText value1;
-     EditText value2;
-     Button Calculate;
-     TextView result;
-     Spinner functions_array;
+    private EditText value1;
+    private EditText value2;
+    private TextView result;
+    private Spinner functions_array;
 
-     float number1, number2;
-     float result_number;
+    private float number1;
+    private float number2;
+    private float result_number;
 
 
     @Override
@@ -31,35 +36,40 @@ public class MainActivity extends AppCompatActivity {
 
         value1 = findViewById(R.id.editText);
         value2 = findViewById(R.id.editText2);
-        Calculate = findViewById(R.id.button_calculate);
+        Button calculate = findViewById(R.id.button_calculate);
         result = findViewById(R.id.result);
         functions_array = findViewById(R.id.operation);
 
 
-        Calculate.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if (value1.getText().length() == 0 && value2.getText().length() == 0) {
-                result.setText("Make sure that input Values are not empty");
-        } else {
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (value1.getText().length() == 0 && value2.getText().length() == 0) {
+                    result.setText(R.string.set_text_string);
+                } else {
 
-                number1 = Integer.parseInt(value1.getText().toString());
-                number2 = Integer.parseInt(value2.getText().toString());
+                    number1 = Integer.parseInt(value1.getText().toString());
+                    number2 = Integer.parseInt(value2.getText().toString());
 
-                if (functions_array.getSelectedItem().toString().equals("+")) {
-                    result_number = number1 + number2;
-                } else if (functions_array.getSelectedItem().toString().equals("-")) {
-                    result_number = number1 - number2;
-                } else if (functions_array.getSelectedItem().toString().equals("x")) {
-                    result_number = number1 * number2;
-                } else if (functions_array.getSelectedItem().toString().equals("/")) {
-                    result_number = number1 / number2;
+                    switch (functions_array.getSelectedItem().toString()) {
+                        case "+":
+                            result_number = number1 + number2;
+                            break;
+                        case "-":
+                            result_number = number1 - number2;
+                            break;
+                        case "x":
+                            result_number = number1 * number2;
+                            break;
+                        case "/":
+                            result_number = number1 / number2;
+                            break;
+                    }
+
+                    result.setText(String.valueOf(result_number));
                 }
-
-                result.setText( String.valueOf(result_number));
             }
-            }
-        } );
+        });
 
 
     }
